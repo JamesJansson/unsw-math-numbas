8874b4ef8a66d93168b28185baa7e61655884617
James Jansson <jamesjansson@gmail.com>
2018-08-17 08:17:08 +1000

    Clean up and use better numbers

--- riemann-chart-basic.js
+++ riemann-chart-basic.js
@@ -2,16 +2,13 @@
 // Use student input in a JSXGraph diagram by Christian Lawson-Perfect
 // https://numbas.mathcentre.ac.uk/question/2223/use-student-input-in-a-jsxgraph-diagram/
 
-var div = Numbas.extensions.jsxgraph.makeBoard('400px','400px',
+var div = Numbas.extensions.jsxgraph.makeBoard('400px','400px', 
   {
-    boundingBox: [-2,25,13,-3], // xmin, ymax, xmax, ymin
+    boundingBox: [-2,25, 6,-3], // xmin, ymax, xmax, ymin
     axis: false,
     showNavigation: false,
     grid: true
   });
-
-// board.suspendUpdate(); 
-
 
 var board = div.board;
 
@@ -20,7 +17,7 @@ var xaxis = board.create('line', [[0, 0], [1, 0]],
       strokeColor: 'black',
       fixed: true
     });
-var xticks = board.create('ticks', [xaxis, 2],
+var xticks = board.create('ticks', [xaxis, 1],
     {
       drawLabels: true,
       label: {offset: [-4, -15]},
@@ -39,9 +36,6 @@ var yticks = board.create('ticks', [yaxis, 2],
       minorTicks: 0
     });
 
-// Create bar chart from here
-// http://jsxgraph.uni-bayreuth.de/wiki/index.php/Dynamic_bar_chart
-// http://jsxgraph.uni-bayreuth.de/wiki/index.php/Different_chart_styles
 
 function equation(x) {
   return a*x*x + b*x + c;
@@ -57,24 +51,16 @@ function equation(x) {
 // }
 
 var curveline = board.create('functiongraph',
-                [equation, -2, 4]);
+                [equation, -2, 16]);
 
-// var curveline = board.create('functiongraph', 
-//    [makecurve,-13,13],
-//    {strokeColor:'black', strokeWidth: 3, visible: false}
-//   );
 
-var barWidth = 0.5;
+//var barWidth = 0.5;
 
-var length = 10;
+var length = 5;
 
 var x = [];
 for (var i=0; i<length/barWidth; i++) {
-  
   x.push(i*barWidth);
-  
-  
-  
 }
 
 //var fbar = [5, 6, 7, 8];
@@ -102,29 +88,9 @@ x.forEach(function(xVal){
 });
 
 
-// var chart = board.createElement('chart', [fbar], 
-//     {chartStyle:'bar', width: barWidth, labels:labels, strokeColor: '#0000FF', strokeWidth:'3px'});
-
-
-// for polygons
-// x.forEach
-// i=0;
-// var p1 = board.create('point', [x[i], fbar[i]], {size:0, strokeColor:'blue'});
-// var p2 = board.create('point', [x[i], 0], {size:0, strokeColor:'blue'});
-// var p3 = board.create('point', [x[i] + barWidth, 0], {size:0, strokeColor:'blue'});
-// var p4 = board.create('point', [x[i] + barWidth, fbar[i]], {size:0, strokeColor:'blue'});
-
-// var poly = board.createElement('polygon',[p1, p2, p3, p4]);
-
 // http://jsxgraph.uni-bayreuth.de/wiki/index.php/Polygon
 
 
-    //, colorArray:['#B02B2C','#3F4C6B','#C79810','#D15600']}); // try color, or maybe repeat colour
-
 // Perform a definite integral on the function
-
-// board.unsuspendUpdate();
-
-// board.update();
 
 return div;
