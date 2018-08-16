4827ecf9d3384a142d49e94ad3ecd44e4cdf56be
James Jansson <jamesjansson@gmail.com>
2018-08-16 18:06:41 +1000

    Add polygons

--- first-chart.js
+++ first-chart.js
@@ -70,21 +70,43 @@ var length = 10;
 
 var x = [];
 for (var i=0; i<length/barWidth; i++) {
+  
   x.push(i*barWidth);
+  
+  
+  
 }
 
 //var fbar = [5, 6, 7, 8];
 
 var fbar = [];
 x.forEach(function(xval){
+  // if left
   fbar.push(equation(xval));
+  // if mid, + 0.5 * barWidth
+  // fbar.push(equation(xval + 0.5 * barWidth));
+  // if mid, + barWidth
+  // fbar.push(equation(xval + barWidth));
 });
 
 labels = fbar * barWidth;
 
 var chart = board.createElement('chart', [fbar], 
-    {chartStyle:'bar', width: barWidth, labels:labels});
-    
+    {chartStyle:'bar', width: barWidth, labels:labels, strokeColor: '#0000FF', strokeWidth:'3px'});
+
+
+// for polygons
+i=0;
+var p1 = board.create('point', [x[i], fbar[i]], {size:0, strokeColor:'blue'});
+var p2 = board.create('point', [x[i], 0], {size:0, strokeColor:'blue'});
+var p3 = board.create('point', [x[i] + barWidth, 0], {size:0, strokeColor:'blue'});
+var p4 = board.create('point', [x[i] + barWidth, fbar[i]], {size:0, strokeColor:'blue'});
+
+var poly = board.createElement('polygon',[p1, p2, p3, p4]);
+
+// http://jsxgraph.uni-bayreuth.de/wiki/index.php/Polygon
+
+
     //, colorArray:['#B02B2C','#3F4C6B','#C79810','#D15600']}); // try color, or maybe repeat colour
 
 // Perform a definite integral on the function
