ac687618cdbda4cf48abedf797059f68dfae8247
James Jansson <jamesjansson@gmail.com>
2018-08-17 23:42:15 +1000

    Add range calculator

--- riemann-chart-basic.js
+++ riemann-chart-basic.js
@@ -2,9 +2,41 @@
 // Use student input in a JSXGraph diagram by Christian Lawson-Perfect
 // https://numbas.mathcentre.ac.uk/question/2223/use-student-input-in-a-jsxgraph-diagram/
 
+// Get equation, interpret
+
+
+// Determine the rough min and max range, given the domain of the question
+var length = x_1 - x_0;
+xTestStep = length / 10;
+for (var xTest = x_0; v <= x_1; xTest += xTestStep) {
+  var fval = f(xTest);
+  if (fMax === undefined) {
+    fMax = fVal;
+  }
+  if (fMin === undefined) {
+    fMin = fVal;
+  }
+  if (fVal > fMax) {
+    fMax = fVal;
+  }
+  if (fVal < fMin) {
+    fMin = fVal;
+  }
+}
+
+yBoundMax = Math.ceil(fMax + (fMax - fMin) / 10);
+yBoundMax = yBoundMax < 0 ? 0 : yBoundMax;
+yBoundMin = Math.floor(fMax - (fMax - fMin) / 10);
+yBoundMin = yBoundMin > 0 ? 0 : yBoundMin;
+
+xBoundMax = Math.ceil(fMax + (fMax - fMin) / 10);
+xBoundMax = xBoundMax < 0 ? 0 : xBoundMax;
+xBoundMin = Math.floor(fMax - (fMax - fMin) / 10);
+xBoundMin = xBoundMin > 0 ? 0 : xBoundMin;
+
 var div = Numbas.extensions.jsxgraph.makeBoard('400px','400px', 
   {
-    boundingBox: [-2,25, 6,-3], // xmin, ymax, xmax, ymin
+    boundingBox: [xBoundMin, yBoundMax, xBoundMax, yBoundMin], // xmin, ymax, xmax, ymin
     axis: false,
     showNavigation: false,
     grid: true
@@ -29,7 +61,7 @@ var yaxis = board.create('line', [[0, 0], [0, 1]],
       strokeColor: 'black',
       fixed: true
     });
-var yticks = board.create('ticks', [yaxis, 2],
+var yticks = board.create('ticks', [yaxis, 1],
     {
       drawLabels: true,
       label: {offset: [-20, 0]},
@@ -39,24 +71,16 @@ var yticks = board.create('ticks', [yaxis, 2],
 
 function equation(x) {
   return a*x*x + b*x + c;
+  // Create a JME scope with the variable x set to the given value.
+  // var nscope = new Numbas.jme.Scope([
+  //     Numbas.jme.builtinScope,
+  //     {variables: {x: new Numbas.jme.types.TNum(x)}}
+  // ]);
+  // return Numbas.jme.evaluate(equation, nscope).value;
 }
-
-// function makecurve(x) {
-//   // Create a JME scope with the variable x set to the given value.
-//   var nscope = new Numbas.jme.Scope([
-//       Numbas.jme.builtinScope,
-//       {variables: {x: new Numbas.jme.types.TNum(x)}}
-//   ]);
-//   return Numbas.jme.evaluate(equation, nscope).value;
-// }
 
 var curveline = board.create('functiongraph',
                 [equation, -2, 16]);
-
-
-//var barWidth = 0.5;
-
-var length = 5;
 
 var x = [];
 for (var i=0; i<length/barWidth; i++) {
