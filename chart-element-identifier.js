21bcf50fb7f881ce93b4e5d56656c60a07c89356
James Jansson <jamesjansson@gmail.com>
2018-09-09 23:30:46 +1000

    Add old script

---
+++ chart-element-identifier.js
@@ -0,0 +1,136 @@
+// Get equation, interpret
+// var equationString = question.parts[0].gaps[0].display.studentAnswer();
+var compiledExpression = Numbas.jme.compile(equationString, scope);
+if(compiledExpression === null) {
+  throw(new Error('No equation'));
+}
+
+console.log(Numbas.exam.currentQuestion)
+
+console.log(Numbas.jme.variables);
+//Numbas.jme.variables.makeVariables(['testmv1', 'testmv2'], Numbas.jme.builtinScope);
+
+function equation(x) {
+  // Create a JME scope with the variable x set to the given value.
+  var nscope = new Numbas.jme.Scope([
+      Numbas.jme.builtinScope,
+      {
+        variables: {
+          x: new Numbas.jme.types.TNum(x),
+          a: new Numbas.jme.types.TNum(a),
+          b: new Numbas.jme.types.TNum(b),
+          c: new Numbas.jme.types.TNum(c),
+          c: new Numbas.jme.types.TNum(d)
+        }
+      }
+  ]);
+  return Numbas.jme.evaluate(compiledExpression, nscope).value;
+}
+
+// Determine the rough min and max range, given the domain of the question
+var length = x_1 - x_0;
+var xTestStepSize = length / 10;
+var fMin, fMax;
+for (var xTest = x_0; xTest <= x_1; xTest += xTestStepSize) {
+  var fVal = equation(xTest);
+  if (fMax === undefined) {
+    fMax = fVal;
+  }
+  if (fMin === undefined) {
+    fMin = fVal;
+  }
+  if (fVal > fMax) {
+    fMax = fVal;
+  }
+  if (fVal < fMin) {
+    fMin = fVal;
+  }
+}
+var yBuffer = (fMax - fMin) / 7;
+yBoundMax = Math.ceil(fMax + yBuffer);
+yBoundMax = yBoundMax < yBuffer ? yBuffer : yBoundMax;
+yBoundMin = Math.floor(fMin - yBuffer);
+yBoundMin = yBoundMin > -yBuffer ? -yBuffer : yBoundMin;
+
+var xBuffer = (x_1 - x_0) / 7;
+xBoundMax = Math.ceil(x_1 + xBuffer);
+xBoundMax = xBoundMax < xBuffer ? xBuffer : xBoundMax;
+xBoundMin = Math.floor(x_0 - xBuffer);
+xBoundMin = xBoundMin > -xBuffer ? -xBuffer : xBoundMin;
+
+var div = Numbas.extensions.jsxgraph.makeBoard('400px','400px', 
+  {
+    boundingBox: [xBoundMin, yBoundMax, xBoundMax, yBoundMin], // xmin, ymax, xmax, ymin
+    axis: false,
+    showNavigation: false,
+    grid: false
+  });
+
+var board = div.board;
+
+function optimalTickDistance(minVal, maxVal, targetTicks) {
+  // optimal ticks should be between 10 and 25
+  var dist = maxVal -minVal;
+  // Determine optimal ticks
+  var targetTickDist = dist/targetTicks;
+  var targetLog10 = Math.log(targetTickDist) * Math.LOG10E;
+  var exponent = Math.floor(targetLog10);
+  var remainder = targetLog10 - exponent;
+
+  // Try 1
+  var multiple = 1;
+  var bestDiff = Math.abs(Math.log(1) * Math.LOG10E - remainder);
+  // Try 2
+  var tryDiff = Math.abs(Math.log(2) * Math.LOG10E - remainder);
+  if (tryDiff < bestDiff) {
+    bestDiff = tryDiff;
+    multiple = 2;
+  }
+  // Try 5
+  var tryDiff = Math.abs(Math.log(5) * Math.LOG10E - remainder);
+  if (tryDiff < bestDiff) {
+    bestDiff = tryDiff;
+    multiple = 5;
+  }
+  // Try 10
+  var tryDiff = Math.abs(Math.log(10) * Math.LOG10E - remainder);
+  if (tryDiff < bestDiff) {
+    bestDiff = tryDiff;
+    multiple = 10;
+  }
+  return multiple * Math.pow(10, exponent);
+}
+
+var xaxis = board.create('line', [[0, 0], [1, 0]],
+    {
+      strokeColor: 'black',
+      fixed: true
+    });
+var xticks = board.create('ticks', [xaxis, optimalTickDistance(xBoundMin, xBoundMax, 10)],
+    {
+      drawLabels: true,
+      label: {offset: [-4, -15]},
+      minorTicks: 0
+    });
+
+var yaxis = board.create('line', [[0, 0], [0, 1]], 
+    {
+      strokeColor: 'black',
+      fixed: true
+    });
+var yticks = board.create('ticks', [yaxis,  optimalTickDistance(yBoundMin, yBoundMax, 20)],
+    {
+      drawLabels: true,
+      label: {offset: [-20, 0]},
+      minorTicks: 0
+    });
+
+var curveline = board.create('functiongraph',
+    [equation, xBoundMin, xBoundMax]);
+
+var g1 = board.create('glider', [0.6, 1.2, curveline]);
+var t1 = board.create('tangent', [g1]);
+
+// http://jsxgraph.uni-bayreuth.de/wiki/index.php/Polygon
+
+return div;
