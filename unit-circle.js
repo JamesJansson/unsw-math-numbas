739b6447b43fc4212c325a14f43bf04e1ed5ceca
James Jansson <jamesjansson@gmail.com>
2018-09-13 05:54:19 +1000

    Circle works

--- unit-circle.js
+++ unit-circle.js
@@ -16,23 +16,29 @@ var board = div.board;
 var p1 = board.create('point', [0, 0], {name: ''});
 p1.setProperty({fixed:true});
 var circle = board.create('circle', [p1, 1]); // [0,0]
-var g1 = board.create('glider', [circle]);
+var g1 = board.create('glider', [0.5, Math.sqrt(1 - Math.pow(0.5, 2)),  circle]);
 
 var l1 = board.create('line', [p1, g1]);
 
-var theta = Math.acos(g1.X());
-// If it is on the opposite side of the circle
-if (g1.Y() > 0) {
-  theta = 2 * Math.PI - theta;
-}
 
-var thetaDisplay = Math.round(theta/Math.PI * 100) / 100 + ' PI';
-var p2 = board.create('point', [0, 0], {name: thetaDisplay});
+
+// var thetaDisplay = Math.round(theta/Math.PI * 100) / 100 + ' PI';
+var p2 = board.create('point', [0, 0], {name: 'theta'});
 p2.setProperty({fixed:true});
 
 
 
 var func = function(t){ return Math.sin(t);}
+
+var thetaFunc = function () {
+  var theta = Math.acos(g1.X());
+  // If it is on the opposite side of the circle
+  if (g1.Y() < 0) {
+    theta = 2 * Math.PI - theta;
+  }
+  return theta;
+};
+
 
 var graph = board.create('curve',
     [function(t){ return 1 + spacing + t},
@@ -40,10 +46,10 @@ var graph = board.create('curve',
     0, 2*Math.PI]
 );
 
-var p3 = board.create('point', [theta, func(theta)], {name: ''});
-p3.setProperty({fixed:true});
-var p4 = board.create('point', [theta, 0], {name: ''});
-p4.setProperty({fixed:true});
+var p3 = board.create('point', [function(){return 1 + spacing + thetaFunc()}, function(){return func(thetaFunc())}], {name: ''});
+// p3.setProperty({fixed:true});
+var p4 = board.create('point', [function(){return 1 + spacing + thetaFunc()}, 0], {name: ''});
+// p4.setProperty({fixed:true});
 
 var l2 = board.create('line', [p3, p4]);
 
