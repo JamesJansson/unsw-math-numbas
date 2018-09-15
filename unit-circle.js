0f7a3d49a3ff3e10024ec4f23844f3cacd63b596
James Jansson <jamesjansson@gmail.com>
2018-09-16 01:28:36 +1000

    Adding arc, generalise foramtting

--- unit-circle.js
+++ unit-circle.js
@@ -2,12 +2,20 @@
 
 var spacing = 1; // space between the circle and the graph
 
+var axisStyle = {strokeColor: '#b2b2b2', straightFirst:false, straightLast:false, fixed:true};
+var axisPointStyle = {size:0, strokeWidth:3, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true};
+var axisPointStyleXLabel = {size:0, strokeWidth:0, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true};
+var axisPointStyleYLabel = {size:0, strokeWidth:0, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true, label:{offset: [-20, 0]}};
+var axisPointStyleBelow = {size:0, strokeWidth:3, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true, label:{offset: [-5, -13]}};
+var axisPointStyleLeft = {size:0, strokeWidth:3, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true, label:{offset: [-20, 0]}};
+var arcPointStyle = {name: '', size:0, strokeWidth:3, strokeColor: '#b2b2b2', fillColor: '#b2b2b2', fixed:true};
+
 // Ratio calc
 // y = 1.4 + 1.4 = 2.8
 // x = 1.4 + (1+spacing+2*Math.PI+0.4) = 10.083185307179587
-// 900 * 2.8 / 10.083185307179587 = 250
+// 1080 * 2.8 / 10.083185307179587 = 300
 
-var div = Numbas.extensions.jsxgraph.makeBoard('900px','250px', 
+var div = Numbas.extensions.jsxgraph.makeBoard('1080px','300px', 
   {
     boundingBox: [-1.4, 1.4, 1+spacing+2*Math.PI+0.4, -1.4], // xmin, ymax, xmax, ymin
     axis: false,
@@ -18,25 +26,27 @@ var div = Numbas.extensions.jsxgraph.makeBoard('900px','250px',
 
 var board = div.board;
 
-
+// LEFT CHART
 var p1 = board.create('point', [0, 0], {name: ''});
 p1.setProperty({fixed:true});
 var circle = board.create('circle', [p1, 1]); // [0,0]
 var g1 = board.create('glider', [0.5, Math.sqrt(1 - Math.pow(0.5, 2)),  circle], {name: 'Drag this point'});
 
 var l1 = board.create('line', [p1, g1], {straightFirst:false, straightLast:false});
+var arcPoint1 = board.create('point', [0.75, 0], arcPointStyle);
+var arcPoint2 = board.create('point', [function () {return 0.75*g1.X()}, function () {return 0.75*g1.Y()}], arcPointStyle);
+board.create('arc', [p1, arcPoint1, arcPoint2], {strokeColor: '#b2b2b2'});
 
 
-
-// var thetaDisplay = Math.round(theta/Math.PI * 100) / 100 + ' PI';
-var p2 = board.create('point', [0, 0], {name: 'θ'});
-p2.setProperty({fixed:true});
-
+var thetaDisplay = function () { return 'θ=' + Math.round(thetaFunc()/Math.PI * 100) / 100 + 'π'};
+var p3 = board.create('point', [0, 0], {name: thetaDisplay, fixed:true});
+p3.setProperty({fixed:true});
 
 
+// RIGHT CHART
 var func = function(t){ return Math.sin(t);}
 
-var thetaFunc = function () {
+function thetaFunc () {
   var theta = Math.acos(g1.X());
   // If it is on the opposite side of the circle
   if (g1.Y() < 0) {
@@ -55,20 +65,16 @@ var graph = board.create('curve',
     0, 2*Math.PI]
 );
 
-var p3 = board.create('point', [function(){return thetaOffSet(thetaFunc())}, function(){return func(thetaFunc())}], {name: ''});
-// p3.setProperty({fixed:true});
+var curvePoint = board.create('point', [function(){return thetaOffSet(thetaFunc())}, function(){return func(thetaFunc())}], {name: ''});
 var p4 = board.create('point', [function(){return thetaOffSet(thetaFunc())}, 0], {name: ''});
-// p4.setProperty({fixed:true});
 
-var l2 = board.create('line', [p3, p4], {straightFirst:false, straightLast:false});
+var l2 = board.create('line', [curvePoint, p4], {straightFirst:false, straightLast:false});
 
 
 // draw cross-graph dotted lines (only for sine)
-var l3 = board.create('line', [g1, p3], {straightFirst:false, straightLast:false, dash:2});
+var l3 = board.create('line', [g1, curvePoint], {straightFirst:false, straightLast:false, dash:2});
 
 // draw axes
-var axisStyle = {straightFirst:false, straightLast:false, fixed:true};
-var axisPointStyle = {size:0, strokeWidth:3, strokeColor: '#0000FF', fillColor: '#0000FF', fixed:true};
 var lCircleXAxis = board.create('line', [[-1.2, 0], [1.2, 0]], axisStyle);
 var lCircleYAxis = board.create('line', [[0, -1.2], [0, 1.2]], axisStyle);
 
@@ -80,27 +86,34 @@ var pCircleYAxisNeg = board.create('point', [-1, 0], axisPointStyle);
 pCircleYAxisNeg.setProperty({name:'-1'});
 var pCircleYAxisPos = board.create('point', [1, 0], axisPointStyle);
 pCircleYAxisPos.setProperty({name:'1'});
+var pCurveYAxis1n = board.create('point', [1.2, 0], axisPointStyleXLabel);
+pCurveYAxis1n.setProperty({name:'x'});
+var pCurveYAxis1n = board.create('point', [0, 1.2], axisPointStyleYLabel);
+pCurveYAxis1n.setProperty({name:'y'});
 
 var lCurveXAxis = board.create('line', [[thetaOffSet(-0.2), 0], [thetaOffSet(2*Math.PI +1.2), 0]], axisStyle);
 var lCurveYAxis = board.create('line', [[thetaOffSet(0), -1.2], [thetaOffSet(0), 1.2]], axisStyle);
 
 
 
-var pCurveYAxisNeg = board.create('point', [thetaOffSet(0), -1], axisPointStyle);
+var pCurveYAxisNeg = board.create('point', [thetaOffSet(0), -1], axisPointStyleLeft);
 pCurveYAxisNeg.setProperty({name:'-1'});
-var pCurveYAxisPos = board.create('point', [thetaOffSet(0), 1], axisPointStyle);
+var pCurveYAxisPos = board.create('point', [thetaOffSet(0), 1], axisPointStyleLeft);
 pCurveYAxisPos.setProperty({name:'1'});
-var pCurveXAxis1Pi = board.create('point', [thetaOffSet(Math.PI/2), 0], axisPointStyle);
+var pCurveXAxis1Pi = board.create('point', [thetaOffSet(Math.PI/2), 0], axisPointStyleBelow);
 pCurveXAxis1Pi.setProperty({name:'π/2'});
-var pCurveXAxis1Pi = board.create('point', [thetaOffSet(Math.PI), 0], axisPointStyle);
+var pCurveXAxis1Pi = board.create('point', [thetaOffSet(Math.PI), 0], axisPointStyleBelow);
 pCurveXAxis1Pi.setProperty({name:'π'});
-var pCurveXAxis2Pi = board.create('point', [thetaOffSet(3*Math.PI/2), 0], axisPointStyle);
+var pCurveXAxis2Pi = board.create('point', [thetaOffSet(3*Math.PI/2), 0], axisPointStyleBelow);
 pCurveXAxis2Pi.setProperty({name:'3π/2'});
-var pCurveXAxis2Pi = board.create('point', [thetaOffSet(2*Math.PI), 0], axisPointStyle);
+var pCurveXAxis2Pi = board.create('point', [thetaOffSet(2*Math.PI), 0], axisPointStyleBelow);
 pCurveXAxis2Pi.setProperty({name:'2π'});
 
 
-var pCurveYAxis1n = board.create('point', [thetaOffSet(2*Math.PI + 0.2), 0], {name: 'θ'});
+var pCurveYAxis1n = board.create('point', [thetaOffSet(2*Math.PI + 0.2), 0], axisPointStyleXLabel);
+pCurveYAxis1n.setProperty({name:'θ'});
+var pCurveYAxis1n = board.create('point', [thetaOffSet(0), 1.2], axisPointStyleYLabel);
+pCurveYAxis1n.setProperty({name:'y'});
 
 
 
